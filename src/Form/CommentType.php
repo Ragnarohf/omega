108f863a33c835fbd863f7663f11e4ad8adb605b
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-05-20 14:17:50 +0200

    mise en place de CkEditor

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,8 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content')
-            ->add('createdAt')
-        ;
+            ->add('content', CKEditorType::class)
+            ->add('createdAt');
     }
 
     public function configureOptions(OptionsResolver $resolver)
